69e25dde2aec3e6bccc848c7a921382ea4db1c7d
142432 <111173364+142432@users.noreply.github.com>
2022-08-27 23:31:46 +0200

    Add files via upload

---
+++ dog.js
@@ -0,0 +1,53 @@
+let ess = document.querySelector(".es");
+
+
+ess.onclick = function () {
+    window.scrollTo(0,940);
+
+}
+
+
+
+let ed = document.querySelector(".ed");
+
+
+ed.onclick = function () {
+    window.scrollTo(0,2220);
+
+}
+
+
+let edd = document.querySelector(".edd");
+
+
+edd.onclick = function () {
+    window.scrollTo(0,2220);
+
+}
+
+
+
+let ef = document.querySelector(".ef");
+
+
+ef.onclick = function () {
+    window.scrollTo(0,940);
+
+}
+
+
+
+let eg = document.querySelector(".eg");
+
+
+eg.onclick = function () {
+    window.scrollTo(0,3400);
+
+}
+/*
+
+let pe = document.querySelector(".pe");
+
+pe.onclick = function () {
+    alert("<h1>German Seberd</h1><br><p>German is haig price is found none picccccccccccckw <br> price :2000Le</p>")
+}*/
